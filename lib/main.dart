bc1ad8b6e9f4282e4f1abf9643a3072f05963674
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-09-19 13:07:17 -0300

    Layalt das mesnagens e layalt do textfild para enviar mensagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -22,8 +23,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Chat App",
       debugShowCheckedModeBanner: false,
-      theme: Theme.of(context).platform == TargetPlatform.iOS ?
-          IOSTheme : AndroidDefault,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? IOSTheme
+          : AndroidDefault,
       home: ChatScreen(),
     );
   }
@@ -44,10 +46,23 @@ class _ChatScreenState extends State<ChatScreen> {
         appBar: AppBar(
           title: Text("Chat App"),
           centerTitle: true,
-          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
         body: Column(
           children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage(),
+                ],
+              ),
+            ),
+            Divider(
+              height: 1.0,
+            ),
             Container(
               decoration: BoxDecoration(
                 color: Theme.of(context).cardColor,
@@ -61,15 +76,95 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
+
+//Icone camera, textfild para enviar mensagens e icone de enviar
 class TextCompose extends StatefulWidget {
   @override
   _TextComposeState createState() => _TextComposeState();
 }
 
 class _TextComposeState extends State<TextCompose> {
+  bool _isCompsing = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isCompsing = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoButton(
+                        child: Text("Enviar"),
+                        onPressed: _isCompsing ? () {} : null,
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.send),
+                        onPressed: _isCompsing ? () {} : null,
+                      ))
+          ],
+        ),
+      ),
+    );
   }
 }
 
+//imagem do usuario da mensagem com sua respectiva mensagem
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://i.ibb.co/PQ74bwj/ft1.jpg"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Ricardo",
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("teste"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
